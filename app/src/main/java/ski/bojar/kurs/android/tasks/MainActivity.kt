5ab52827700992667317fd27a5e1e2c2fc9c07bf
bojar.ski <bojar.ski>
2023-04-12 22:42:22 +0200

    Witaj Androidzie

--- app/src/main/java/ski/bojar/kurs/android/tasks/MainActivity.kt
+++ app/src/main/java/ski/bojar/kurs/android/tasks/MainActivity.kt
@@ -3,12 +3,13 @@ package ski.bojar.kurs.android.tasks
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.material3.Text
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-
+            Text(text = "Witaj Androidzie!")
         }
     }
 }
