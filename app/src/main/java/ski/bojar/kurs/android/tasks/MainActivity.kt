4f1954509015cecf655f4d18ef163fdf59550ccc
bojar.ski <bojar.ski>
2023-04-12 22:43:15 +0200

    Compose - function

--- app/src/main/java/ski/bojar/kurs/android/tasks/MainActivity.kt
+++ app/src/main/java/ski/bojar/kurs/android/tasks/MainActivity.kt
@@ -4,12 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Text(text = "Witaj Androidzie!")
+            MyElement()
         }
+    }
+
+    @Composable
+    fun MyElement() {
+        Text(text = "Witaj Androidzie!")
     }
 }
