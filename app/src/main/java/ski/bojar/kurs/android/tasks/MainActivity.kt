b5d94b5f96c61a3cc87f63601df7e5ee85dda22c
bojar.ski <bojar.ski>
2023-04-13 11:43:57 +0200

    Compose - Column

--- app/src/main/java/ski/bojar/kurs/android/tasks/MainActivity.kt
+++ app/src/main/java/ski/bojar/kurs/android/tasks/MainActivity.kt
@@ -3,19 +3,41 @@ package ski.bojar.kurs.android.tasks
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MyElement()
+            MyColumn()
+        }
+    }
+
+    @Composable
+    fun MyColumn() {
+        Column(
+            modifier = Modifier.background(Color.LightGray).fillMaxSize(),
+            horizontalAlignment = Alignment.Start, // Start, CenterHorizontally, End
+            verticalArrangement = Arrangement.Bottom
+        ) {
+            Text(text = "Witaj Androidzie!")
+            Text(text = "Witaj ponownie!")
+            Text(text = "Hej, co u Ciebie?")
         }
     }
 
     @Composable
     fun MyElement() {
         Text(text = "Witaj Androidzie!")
+        Text(text = "Witaj ponownie!")
+        Text(text = "Hej, co u Ciebie?")
     }
 }
