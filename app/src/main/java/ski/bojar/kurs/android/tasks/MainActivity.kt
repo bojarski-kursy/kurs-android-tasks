674c43290bd33c32ae5f111ffd178661aced83fd
bojar.ski <bojar.ski>
2023-05-15 12:53:57 +0200

    Compose - Button click

--- app/src/main/java/ski/bojar/kurs/android/tasks/MainActivity.kt
+++ app/src/main/java/ski/bojar/kurs/android/tasks/MainActivity.kt
@@ -41,6 +41,11 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.rotate
@@ -58,8 +63,40 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            //MyMusicPlayerView()
-            MyUdemyLayoutExercise()
+            MyClickButton()
+        }
+    }
+
+    //var count: Int = 0
+
+    @Composable
+    fun MyClickButton() {
+        var countState: MutableState<Int> = remember { mutableStateOf(0) }
+        var count: Int by remember { mutableStateOf(0) }
+
+        Column() {
+            Button(onClick = { countState.value++ }) {
+                Text(text = "Click countState ${countState.value}")
+            }
+            Button(onClick = { count++ }) {
+                Text(text = "Click count $count")
+            }
+            MyInnerClickButton(count = count)
+            MyInnerClickStateButton(countState = countState)
+        }
+    }
+    
+    @Composable
+    fun MyInnerClickButton(count: Int) {
+        Button(onClick = { /* count++ */ }) {
+            Text(text = "Inner count $count")
+        }
+    }
+    
+    @Composable
+    fun MyInnerClickStateButton(countState: MutableState<Int>) {
+        Button(onClick = { countState.value++ }) {
+            Text(text = "Inner count state ${countState.value}")
         }
     }
 
