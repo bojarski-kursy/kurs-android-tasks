678175f7977493d05666a347e2a07086b3dbed98
bojar.ski <bojar.ski>
2023-07-21 13:29:44 +0200

    TaskActivity

---
+++ app/src/main/java/ski/bojar/kurs/android/tasks/view/TaskActivity.kt
@@ -0,0 +1,122 @@
+package ski.bojar.kurs.android.tasks.view
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+class TaskActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            TaskView()
+        }
+    }
+
+    @Composable
+    fun TaskView() {
+        val taskColors = listOf(Color.Cyan, Color.Yellow, Color.Magenta)
+
+        var currentColor by remember { mutableStateOf(taskColors.first()) }
+        var titleText by remember { mutableStateOf("") }
+        var descriptionText by remember { mutableStateOf("") }
+        
+        Column(
+            modifier = Modifier.padding(20.dp)
+        ) {
+            Text(text = "Task", fontSize = 30.sp)
+            Spacer(modifier = Modifier.height(20.dp))
+            Card(
+                colors = CardDefaults.cardColors(containerColor = currentColor),
+                elevation = CardDefaults.elevatedCardElevation(defaultElevation = 10.dp)
+            ) {
+                OutlinedTextField(
+                    value = titleText,
+                    onValueChange = { titleText = it },
+                    label = { Text(text = "Title") },
+                    textStyle = TextStyle(fontSize = 20.sp, fontWeight = FontWeight.Bold),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(8.dp)
+                )
+                OutlinedTextField(
+                    value = descriptionText,
+                    onValueChange = { descriptionText = it },
+                    label = { Text(text = "Description") },
+                    textStyle = TextStyle(fontSize = 20.sp, fontWeight = FontWeight.Bold),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(8.dp)
+                )
+            }
+
+            Spacer(modifier = Modifier.height(20.dp))
+
+            LazyRow() {
+                items(items = taskColors) { color ->
+                    Button(
+                        onClick = { currentColor = color},
+                        shape = CircleShape,
+                        colors = ButtonDefaults.buttonColors(containerColor = color),
+                        elevation = ButtonDefaults.elevatedButtonElevation(defaultElevation = 8.dp),
+                        border = BorderStroke(2.dp, if (currentColor == color) Color.Gray else color),
+                        modifier = Modifier
+                            .padding(horizontal = 8.dp)
+                            .size(40.dp)
+                    ) { }
+                }
+            }
+
+            Spacer(modifier = Modifier.height(40.dp))
+
+            Button(
+                onClick = { /*TODO*/ },
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text(text = "Save")
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
