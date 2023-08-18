c411baffa0e00d27ab4b7d6601db4af40ce2fa70
bojar.ski <bojar.ski>
2023-08-18 13:51:33 +0200

    Task - FAB

--- app/src/main/java/ski/bojar/kurs/android/tasks/view/HomeActivity.kt
+++ app/src/main/java/ski/bojar/kurs/android/tasks/view/HomeActivity.kt
@@ -1,11 +1,23 @@
 package ski.bojar.kurs.android.tasks.view
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
 import ski.bojar.kurs.android.tasks.model.Task
 
 class HomeActivity : ComponentActivity() {
@@ -20,6 +32,27 @@ class HomeActivity : ComponentActivity() {
 
         setContent {
             //HomeText(welcomeValue)
+            HomeView()
+        }
+    }
+
+    @Composable
+    fun HomeView() {
+        val context = LocalContext.current
+        Box(
+            modifier = Modifier.fillMaxSize()
+        ) {
+            FloatingActionButton(
+                onClick = {
+                    val intent = Intent(context, TaskActivity::class.java)
+                    startActivity(intent)
+                },
+                modifier = Modifier
+                    .padding(16.dp)
+                    .align(Alignment.BottomEnd)
+            ) {
+                Icon(imageVector = Icons.Default.Add, contentDescription = "add task")
+            }
         }
     }
 
